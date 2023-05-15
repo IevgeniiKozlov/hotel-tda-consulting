3c9670d282cd43e01528a3b3acb306efefe4f0e0
Dmitriy <dima.komlyk@gmail.com>
2023-05-15 13:43:58 +0300

    Created simple footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCopyright } from "@fortawesome/free-solid-svg-icons"
+import {
+  Paper,
+  Container,
+  Box,
+  Typography,
+  Stack
+} from '@mui/material';
+import * as styles from "./footer.module.scss";
+
+export default () => (
+  <Paper className={styles.footer} component="footer">
+    <Container>
+      <Stack direction="row" spacing={1} justifyContent="center" alignItems="center">
+        <FontAwesomeIcon icon={faCopyright} />
+        <Typography>{new Date().getFullYear()} TDA Development</Typography>
+      </Stack>
+    </Container>
+  </Paper>
+);
