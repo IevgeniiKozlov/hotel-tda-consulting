839e848d6adf085f91b67ccdf8886da7bd0cc8de
Kozlov Evgeniy <dp200890kev@gmail.com>
2023-07-05 11:45:04 +0300

    Added footer and header component, mui Box component

--- src/components/project-layout/project-layout.js
+++ src/components/project-layout/project-layout.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { StyledEngineProvider } from '@mui/material/styles';
+import { Box } from '@mui/material';
 import {
   createTheme,
   responsiveFontSizes,
   ThemeProvider,
 } from '@mui/material/styles';
+
+import Header from "../header/header";
+import Footer from "../footer/footer";
+
+import { wrapper } from "./project-layout.module.scss";
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
@@ -13,7 +19,13 @@ const ProjectLayout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <StyledEngineProvider injectFirst>
-        <main>{children}</main>
+        <Box className={wrapper}>
+          <Header />
+            <main>
+              { children }
+            </main>
+          <Footer /> 
+        </Box>
       </StyledEngineProvider>
     </ThemeProvider>
   )
