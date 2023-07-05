c96c53dac6827a1b7a154e2d5df7a7cb732f09ed
Kozlov Evgeniy <dp200890kev@gmail.com>
2023-07-05 11:52:51 +0300

    Added dynamic cards and them styles

--- src/components/swiper/slider-card/slider-card.js
+++ src/components/swiper/slider-card/slider-card.js
@@ -1,46 +1,45 @@
 import * as React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCards } from "swiper";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { Card, CardContent, Typography, CardMedia} from '@mui/material';
+import { useTranslation } from "gatsby-plugin-react-i18next";
 
-// Import Swiper styles
 import "swiper/scss";
 import "swiper/scss/effect-cards";
-
 // Import required modules
 import * as styles from "./slider-card.module.scss";
 
-const SliderCard = ( {data} ) => {
-    
+const SliderCard = ( {images} ) => {
+    const {t} = useTranslation();
+
     return (
         <>
             <Swiper
-            effect={"cards"}
-            grabCursor={true}
-            modules={[EffectCards]}
-            className={styles.mySwiper}
+                effect={"cards"}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className={styles.mySwiper}
             >
-                {data.map(({title, description, image}) => (
-                    <SwiperSlide key={image.src.id} className={styles.slide}>
+                {images.map(({id, src, alt}) => {
+                   const { title } = t('greeting.cards', { returnObjects: true }).find(card => card.id === id)
+                    return (
+                    <SwiperSlide key={id} className={styles.slide}>
                         <Card className={styles.card}>
                             <CardMedia>
                                 <GatsbyImage 
-                                    alt={image.alt}
-                                    image={image.src.childImageSharp.gatsbyImageData}
-                                />
+                                    alt={alt}
+                                    image={getImage(src)}
+                                />  
                             </ CardMedia>
                             <CardContent className={styles.cardContent}>
                                 <Typography className={styles.text} gutterBottom variant="h5" component="span">
                                     {title}
                                 </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {description}
-                                </Typography>
                             </CardContent>
                         </Card>
                     </SwiperSlide> 
-                ))}
+                )})}
             </Swiper>
         </>
 
