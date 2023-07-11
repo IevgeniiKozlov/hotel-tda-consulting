08af0d0c691cdfb3b8fa474a726b0da6a2933c7c
Kozlov Evgeniy <dp200890kev@gmail.com>
2023-07-11 19:45:54 +0300

    Added scroll on top page

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+export function shouldUpdateScroll(prevRouterProps, { location }) {
+    window.scrollTo(0, 0)
+    const body = document.getElementsByTagName('body')[0]
+    body.scrollTop = 0
+    return false
+}
